524c34af8a5eeda81b8d693bfa16bdb63ba8fc20
hai <quanghai27298@gmail.com>
2021-04-08 18:12:54 +0700

    bai 2

--- Test.php
+++ Test.php
@@ -18,4 +18,36 @@
     var_dump(struct_array(3 , [11, 2, 8, 10, 5, 99, 1, 8, 9]));
 
 
-    echo 'jskasj';
+    //Bài 2 tổng 2 số lớn nhất
+    function sum_two_largest_number($arr)
+    {
+        $mang = $arr;
+        $max =0; $beformax = 0;
+        if($mang[0] > $mang[1])
+        {
+            $max = $mang[0];
+            $beformax = $mang[1];
+        }
+        else{
+            $max = $mang[1];
+            $beformax = $mang[0];
+        }
+
+        for ($i = 2; $i <count($mang); $i++) {
+            if($mang[$i] > $max)
+            {
+                $beformax = $max;
+                $max = $mang[$i];
+            }
+            else if($mang[$i] >$beformax)
+            {
+                $beformax = $mang[$i];
+            }
+        }
+
+        return $max + $beformax;
+
+    }
+
+    echo "<br/>";
+    echo sum_two_largest_number([0, 100, -4, 8, 143, 5, 99, 100]);
