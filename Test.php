0b5b13175e62252b6c8615ddefb317f8bcdfee8b
hai <haiqb@lionnix.vn>
2021-04-08 17:15:05 +0700

    bài 1

---
+++ Test.php
@@ -0,0 +1,18 @@
+<?php
+    //Bài 1 viết lại cấu trúc mảng
+    function struct_array($n, $arr)
+    {
+        $m = count($arr)/$n;
+        $a = array();
+        for($i = 0; $i<$m; $i++)
+        {
+            for($j = 1; $j <=$n; $j++)
+            {
+                $a[] = $arr[$i + $j*$m - $m];
+            }
+        }
+
+        return $a;
+    }
+
+    var_dump(struct_array(3 , [11, 2, 8, 10, 5, 99, 1, 8, 9]));
